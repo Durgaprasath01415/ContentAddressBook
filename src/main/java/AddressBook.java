74ec0510e830fdb07be5db49c697aa644d3da7c3
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-23 23:22:36 +0530

    ADDED:Contents of Address Book

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -21,13 +21,6 @@ public class AddressBook {
         System.out.println("Enter email : ");
 
         System.out.println("Firstname : " + contact.getFirstName() + "\nLastname : " + contact.getLastName() + "\nAddress : " + contact.getAddress() + "\nCity : " + contact.getCity() + "\nState : " + contact.getState() + "\nZip : " + contact.getZip() + "\nPhonenumber : " + contact.getPhoneNumber() + "\nEmail : " + contact.getEmail());
-        PrintStream file = null;
-        try {
-            file = new PrintStream("out.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        System.setOut(file);
-        System.out.println("Firstname : " + contact.getFirstName() + "\nLastname : " + contact.getLastName() + "\nAddress : " + contact.getAddress() + "\nCity : " + contact.getCity() + "\nState : " + contact.getState() + "\nZip : " + contact.getZip() + "\nPhonenumber : " + contact.getPhoneNumber() + "\nEmail : " + contact.getEmail());
+       Address : " + contact.getAddress() + "\nCity : " + contact.getCity() + "\nState : " + contact.getState() + "\nZip : " + contact.getZip() + "\nPhonenumber : " + contact.getPhoneNumber() + "\nEmail : " + contact.getEmail());
     }
 }
