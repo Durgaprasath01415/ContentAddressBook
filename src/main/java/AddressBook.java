28d17d6b9cbdb09ac6837a42fa3b6865db32e4e6
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-23 23:18:15 +0530

    MODIFIED:To get input from user and save in file

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -8,18 +8,35 @@ import java.util.Scanner;
 
 public class AddressBook {
     Contact contact;
+    Scanner scanner = new Scanner(System.in);
 
     public void add() {
         contact = new Contact();
         System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
         System.out.println("Enter last name: ");
+        String lastName = scanner.nextLine();
         System.out.println("Enter address : ");
+        String address = scanner.nextLine();
         System.out.println("Enter city : ");
+        String city = scanner.nextLine();
         System.out.println("Enter state : ");
+        String state = scanner.nextLine();
         System.out.println("Enter zip : ");
+        String zip = scanner.nextLine();
         System.out.println("Enter phonenumber : ");
+        String phoneNumber = scanner.nextLine();
         System.out.println("Enter email : ");
+        String email = scanner.nextLine();
 
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setEmail(email);
         System.out.println("Firstname : " + contact.getFirstName() + "\nLastname : " + contact.getLastName() + "\nAddress : " + contact.getAddress() + "\nCity : " + contact.getCity() + "\nState : " + contact.getState() + "\nZip : " + contact.getZip() + "\nPhonenumber : " + contact.getPhoneNumber() + "\nEmail : " + contact.getEmail());
         PrintStream file = null;
         try {
@@ -29,5 +46,6 @@ public class AddressBook {
         }
         System.setOut(file);
         System.out.println("Firstname : " + contact.getFirstName() + "\nLastname : " + contact.getLastName() + "\nAddress : " + contact.getAddress() + "\nCity : " + contact.getCity() + "\nState : " + contact.getState() + "\nZip : " + contact.getZip() + "\nPhonenumber : " + contact.getPhoneNumber() + "\nEmail : " + contact.getEmail());
+
     }
 }
