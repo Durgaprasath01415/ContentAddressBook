0011aa7dff9f05f2bd862ff4158bfee77be09851
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-24 17:14:23 +0530

    ADDED: The ability to delete

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -5,11 +5,13 @@ public class AddressBookMain {
         System.out.println("Welcome to Address Book Program");
         AddressBook addressBook = new AddressBook();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your choice 1.Add Contact 2.Edit Contact :");
+        System.out.println("Enter your choice 1.Add Contact 2.Edit 3.Delete :");
         int choice = Integer.valueOf(scanner.nextLine());
         switch (choice) {
             case 1:addressBook.add();
             case 2:addressBook.edit();
+            case 3:addressBook.delete();
+
         }
     }
 }
